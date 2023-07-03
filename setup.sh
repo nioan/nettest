df446509a8a6bb1c97550c25ce79fcfdb8d5021f
nioan <nick.ioannou@gmail.com>
2023-07-03 04:42:34 +0100

    Apt get instead of pip for large packages

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@
 sudo apt-get update
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
+# Numpy is too slow to install with pip, so we use apt-get
+sudo apt-get install python3-numpy
 sudo pip3 install -r requirements.txt
