70d037efbb512795c06fa5aa286906c3b3c38716
nioan <nick.ioannou@gmail.com>
2023-07-03 05:08:32 +0100

    fix user on crontab

--- install_cron.py
+++ install_cron.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 from crontab import CronTab
+import getpass
 from config import read_nettest_config
 
 
@@ -8,17 +9,20 @@ def setup_cron_job(script_name, cron_interval):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     script_path = os.path.join(script_dir, script_name)
 
+    # Get the current user's username
+    username = getpass.getuser()
+
     # Get the user's crontab
-    cron = CronTab(user=True)
+    cron = CronTab(user=username)
 
     # Check if a cron job already exists for the script
     for job in cron:
-        if job.command == f"python3 {script_path}":
+        if job.command == f"/usr/bin/python3 {script_path}":
             print("Cron job already set up.")
             return
 
     # Create a new cron job for the script
-    job = cron.new(command=f"python3 {script_path}")
+    job = cron.new(command=f"/usr/bin/python3 {script_path}")
     job.setall(cron_interval)
     cron.write()
     print("Cron job successfully set up.")
